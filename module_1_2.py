8b7490bf22f45f79abc1e08b9538806d678f0b6b
IlyaFilev <rednif77@gmail.com>
2024-09-19 00:01:14 +0300

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,5 @@
+The_number_of_completed_DZ = 12
+Number_of_hours_spend = 1.5
+Cours_name = ('Python,')
+Time_for_one_task = (Number_of_hours_spend / The_number_of_completed_DZ)
+print(Cours_name,'всего задач:', The_number_of_completed_DZ, 'затрачено часов:', Number_of_hours_spend, 'среднее время выполнения', Number_of_hours_spend / The_number_of_completed_DZ)
