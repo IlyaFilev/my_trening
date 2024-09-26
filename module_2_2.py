d2957c962e2c7c2e60ab633a6f751dc058da778a
IlyaFilev <rednif77@gmail.com>
2024-09-26 19:09:11 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+first = input('Привет, сравним три числа - введи первое целое число: ')
+second = input('введи ещё одно целое число: ')
+third = input('тре целое число: ')
+rez = 'Совпадений:'
+if first == second and second == third:
+    print(rez, '3')
+elif first == second or first == third or second == third:
+    print(rez, '2')
+else:
+    print(rez, '0')
