a68785ce30db28ca4f43dd74761bb6a7f2bda191
IlyaFilev <rednif77@gmail.com>
2024-10-02 07:14:56 +0300

    02/10

--- module_2_4.py
+++ module_2_4.py
@@ -1,15 +1,23 @@
 # Задача "Всё не так уж просто": Д/з на "Цикл for. Элементы списка. Полезные функции в цикле"
+# Используя этот список составьте второй список primes содержащий только простые числа.
+# А так же третий список not_primes, содержащий все не простые числа.
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-is_prime = True # не понимаю, зачем мне эта переменная
+is_prime = True # эта переменная является Флагом (переключателем)
 for i in numbers:
-    if i > 1:
-        for j in range(2, i):
+    if i == 1:
+        continue
+    else:
+        for j in range(2, i): # 2 не проверяется, True не меняется => добавляется в primes
             if i % j == 0:
-                not_primes.append(i)
-                break
+                is_prime = False
+                break   # останавливаем, иначе след значение i может перещёлкнуть переменную
             else:
-                primes.append(i)
+                is_prime = True
+    if is_prime == True:
+        primes.append(i)
+    else:
+        not_primes.append(i)
 print('Primes: ', primes)
 print('Not primes: ', not_primes)
---
+++ module_2_5.py
@@ -0,0 +1,21 @@
+def get_matrix(n, m, value):
+    matrix = []
+    if n <= 0 or m <= 0 or value <= 0:
+        return matrix
+    else:
+        for i in range(n):
+            stolbec = []
+            matrix.append(stolbec)
+            for j in range(m):
+                stolbec.append(value)
+    return matrix
+
+
+Result_1 = get_matrix(2, 2, 88)
+print(Result_1)
+
+Result_2 = get_matrix(3, 5, 0.5)
+print(Result_2)
+
+Result_3 = get_matrix(2, 5, 0)
+print(Result_3)
