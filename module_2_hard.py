c99a69eff1ed84241669715094d35b55eb9365c2
IlyaFilev <rednif77@gmail.com>
2024-10-03 02:06:19 +0300

    "Слишком древний шифр"

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,4 +1,20 @@
 import random
+from http.cookiejar import join_header_words
+from pprint import pprint
 
-a = random.choice(range(3, 21))
-print(win)
+Dano = random.choice(range(3, 21))
+shifr = []
+for i in range(1, 20):
+    for j in range(1, 20):
+            if Dano % (i + j) == 0:
+                if i != j and [i, j] not in shifr and [j, i] not in shifr:
+                    shifr.append([i, j])
+
+print(shifr)
+#print(Dano,'-', .join(shifr))
+Parole = '-'
+for i in range(0, len(shifr)):
+    for j in range(0,2):
+        Parole += str(shifr[i][j])
+
+print(Dano,Parole)
