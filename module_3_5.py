d9665da9cd23f2e441568bc9f0570b147a846b34
IlyaFilev <rednif77@gmail.com>
2024-10-07 01:44:20 +0300

    Рекурсивное умножение цифр (доработано)

--- module_3_5.py
+++ module_3_5.py
@@ -4,10 +4,11 @@ def get_multiplied_digits(number):
     first = int(str_number[0])
     if len(str_number) <= 1:
         return first
+    elif int(str_number[-1]) == 0: # если в number последняя цифра 0, то не учитываем.
+        return first*get_multiplied_digits(int(str_number[1:-1]))
     else: # При преобразовании строки(str) в число(int) первые нули убираются. int('00123') -> 123.
         return first*get_multiplied_digits(int(str_number[1:]))
 
 
-result = (get_multiplied_digits(50505))
+result = (get_multiplied_digits(5505))
 print(result)
-
