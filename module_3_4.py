0f1d784dd04ff3f20190ac581714dbaaa4662f6e
IlyaFilev <rednif77@gmail.com>
2024-10-07 01:06:47 +0300

    Произвольное число параметров - "Однокоренные"

--- module_3_4.py
+++ module_3_4.py
@@ -1,25 +1,28 @@
-# "Произвольное число параметров"
+# Произвольное число параметров - "Однокоренные"
 def single_root_words(root_word, *other_words):
     same_words=[]
     for i in range(len(other_words)):
-        if root_word.lower() in other_words[i].lower():
+        if root_word.lower() in other_words[i].lower() or other_words[i].lower() in root_word.lower():
             same_words.append(other_words[i])
     return same_words
 
 
-result1 = (single_root_words('rich', 'Richiest', 'Griffin', 'orichalcum', 'Richies','apple'))
+result1 = (single_root_words('rich', 'Richiest', 'Griffin', 'orichalcum', 'Richies','apple', 'ric'))
 print(result1)
 result2 = (single_root_words('Cline', 'Inclination', 'Incline', 'recline'))
 print(result2)
+
+
 
 # с оператором count()
 def single_root_words(root_word, *other_words):
     same_words=[]
     for i in range(len(other_words)):
-        a = other_words[i].lower().count(root_word.lower())
-        if a > 0:
+        counter = other_words[i].lower().count(root_word.lower())
+        if counter > 0:
             same_words.append(other_words[i])
+
     return f'{root_word} найден в следующих словах : {same_words}'
 
-print(single_root_words('rich', 'Richiest', 'Griffin', 'orichalcum', 'Richies','apple'))
+print(single_root_words('rich', 'Richiest', 'Griffin', 'orichalcum', 'Richies','apple', 'ric'))
 print(single_root_words('Cline', 'Inclination', 'Incline', 'recline'))
