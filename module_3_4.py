0f08dc72ee124d6a6bfa1fb7b6b4b65e34f2f969
IlyaFilev <rednif77@gmail.com>
2024-10-06 18:55:33 +0300

    "Произвольное число параметров"

--- module_3_4.py
+++ module_3_4.py
@@ -7,8 +7,10 @@ def single_root_words(root_word, *other_words):
     return same_words
 
 
-print(single_root_words('rich', 'Richiest', 'Griffin', 'orichalcum', 'Richies','apple'))
-print(single_root_words('Cline', 'Inclination', 'Incline', 'recline'))
+result1 = (single_root_words('rich', 'Richiest', 'Griffin', 'orichalcum', 'Richies','apple'))
+print(result1)
+result2 = (single_root_words('Cline', 'Inclination', 'Incline', 'recline'))
+print(result2)
 
 # с оператором count()
 def single_root_words(root_word, *other_words):
@@ -17,7 +19,7 @@ def single_root_words(root_word, *other_words):
         a = other_words[i].lower().count(root_word.lower())
         if a > 0:
             same_words.append(other_words[i])
-    return same_words
+    return f'{root_word} найден в следующих словах : {same_words}'
 
 print(single_root_words('rich', 'Richiest', 'Griffin', 'orichalcum', 'Richies','apple'))
 print(single_root_words('Cline', 'Inclination', 'Incline', 'recline'))
