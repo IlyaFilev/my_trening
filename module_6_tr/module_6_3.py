21b86125aad5200b7f7b102d1102e9c4ec0a4346
IlyaFilev <rednif77@gmail.com>
2024-11-06 23:11:53 +0300

    Д/з "Множественное наследование"

--- module_6_tr/module_6_3.py
+++ module_6_tr/module_6_3.py
@@ -4,8 +4,9 @@ class Horse:
     def __init__(self, x_distance=0, sound='Frrr'):
         self.sound = sound
         self.x_distance = x_distance
-        super().__init__() # обращаемся к след. по очереди mro классу - Eagle, для получения атрибутов от "родителя"
-# причем, если написать super сразу после init на 5строке, то sound для объектов Pegasus будет наследоваться из Horse.
+        super().__init__()  # обращаемся к след. по очереди mro классу - Eagle, для получения атрибутов от "родителя"
+
+    # причем, если написать super сразу после init на 5строке, то sound для объектов Pegasus будет наследоваться из Horse.
 
     def run(self, dx):
         self.x_distance += dx
@@ -24,23 +25,24 @@ class Eagle:
 class Pegasus(Horse, Eagle):
 
     def move(self, dx, dy):
-        super().run(dx) # передаем параметры dx, dy в функции родительских классов
+        super().run(dx)  # передаем параметры dx, dy в функции родительских классов
         super().fly(dy)
 
     def get_pos(self):
-        return self.x_distance, self.y_distance # всё, что происходит с этими атрибутами в родительских классах,
+        return self.x_distance, self.y_distance  # всё, что происходит с этими атрибутами в родительских классах,
+
     # мгновенно наследуется дочерним, поэтому здесь возвращаю значения этих атрибутов как объекта класса Pegasus.
 
     def voice(self):
-        print(self.sound) # печать значения унаследованного атрибута sound
+        print(self.sound)  # печать значения унаследованного атрибута sound
 
 
 p1 = Pegasus()
 
-print(p1.get_pos())
+print(p1.get_pos())  # (0, 0)
 p1.move(10, 15)
-print(p1.get_pos())
+print(p1.get_pos())  # (10, 15)
 p1.move(-5, 20)
-print(p1.get_pos())
+print(p1.get_pos())  # (5, 35)
 
-p1.voice()
+p1.voice() # I train, eat, sleep, and repeat
