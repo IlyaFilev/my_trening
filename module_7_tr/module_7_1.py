6c36a538b98338d24ca5585e45950153320512cb
IlyaFilev <rednif77@gmail.com>
2024-11-12 00:25:15 +0300

    Д/з "Режимы открытия файлов"

--- module_7_tr/module_7_1.py
+++ module_7_tr/module_7_1.py
@@ -1,30 +1,5 @@
-from pprint import pprint
-
-class Shop:
-
-    def __init__(self):
-        self.__file_name = 'products.txt'
-
-    def get_products(self):
-        file = open(self.__file_name, 'r')
-        data_products = file.read()
-        file.close()
-        return data_products
-
-    def add(self, *products):
-        for i in products:
-            if products[i] in self.get_products():
-                print(f'Продукт {super.self.name} уже есть в магазине')
-            else:
-                print('Добавка')
-                self.__file_name = 'products.txt'
-                file = open(self.__file_name, 'a')
-                file.write(f'{i}\n')
-                file.close()
-
-
-
-class Product(Shop):
+# Режимы открытия файлов - чтение/запись
+class Product:
 
     def __init__(self, name, weight, category):
         self.name = name
@@ -35,13 +10,34 @@ class Product(Shop):
         return f'{self.name}, {self.weight}, {self.category}'
 
 
+class Shop:
+
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
+    def get_products(self): # получения текущих продуктов в списке файла 'products.txt'
+        file = open(self.__file_name, 'r') # открытие файла в режиме чтения (изменение данных невозможно)
+        data_products = file.read() # передача в data_products всего текста файла в виде строки
+        file.close() # закрытие файла
+        return data_products # возврат строки с данными файла 'products.txt'
+
+    def add(self, *products):
+        file = open(self.__file_name, 'a') # при первом запуске в режиме appand будет создан файл 'products.txt'
+        for i in products:
+            t = str(i) # объект класса Product в строковом представлении - для поиска в строке из метода get_products
+            if t in self.get_products(): # получение списка продуктов из файла и поиск Продукта в нем.
+                print(f'Продукт {t} уже есть в магазине')
+            else:
+                file.write(f'{t}\n') # Добавление продуктов в список файла
+        file.close() # закрытие файла
+
 
 s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-# print(p2) # __str__
+print(p2) # __str__
 
 s1.add(p1, p2, p3)
 
