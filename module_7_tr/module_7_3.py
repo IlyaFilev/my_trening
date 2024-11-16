fdb260c7535ed5cd14147ab489fb8a7e0318c2cc
IlyaFilev <rednif77@gmail.com>
2024-11-16 14:55:43 +0300

    Д/з "Оператор "with".

--- module_7_tr/module_7_3.py
+++ module_7_tr/module_7_3.py
@@ -1,32 +1,44 @@
 # "Оператор "with" - оператор "with". Сначала пишется выражение, затем добавляется «as» и указывается
 # объект, в который всё это сохраняется, после чего выполняется необходимое действие:
-# with EXPR as TARG:
-#   ACTION
-
 class WordsFinder:
 
     def __init__(self, *file_names):
         self.file_names = file_names
-        print(self.file_names)
 
     def get_all_words(self):
+        all_words = {}
         for name in self.file_names:
-            print(name)
-            self.all_words = {}
             with open(name, encoding='utf-8') as file:
                 text = (file.read()).lower()
-                print(text)
-                for p in [',', '.', '=', '!', '?', ';', ':', ' - ']:
-                    if p in text:
-                        text = text.replace(p, '')
-                self.all_words.update({name: text.split()})
-        return print(self.all_words)
+                for simbol in [',', '.', '=', '!', '?', ';', ':', ' - ']:
+                    if simbol in text:
+                        text = text.replace(simbol, '')
+            all_words.update({name: text.split()})
+        return all_words
+
+    def find(self, word):
+        find = {}
+        for elem in self.get_all_words().items():
+            s = 0
+            for i in elem[1]:
+                s += 1
+                if word.lower() == i:
+                    find.update({elem[0]: s})
+                    break
+        return find
+
+    def count(self, word):
+        count = {}
+        for elem in self.get_all_words().items():
+            s = 0
+            for i in elem[1]:
+                if word.lower() == i:
+                    s += 1
+                    count.update({elem[0]: s})
+        return count
 
 
-
-
-
-
-finder2 = WordsFinder('test_file.txt', 'test.txt', 'products.txt')
-
-finder2.get_all_words()
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words()) # Все слова
+print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего
