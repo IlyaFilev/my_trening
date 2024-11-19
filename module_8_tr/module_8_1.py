eefff5c1f505a292a0eff8eb0921c492d39675fe
IlyaFilev <rednif77@gmail.com>
2024-11-19 07:55:33 +0300

    Д/з "Try и Except"

--- module_8_tr/module_8_1.py
+++ module_8_tr/module_8_1.py
@@ -1,13 +1,17 @@
 # "Try и Except"
+
 def add_everything_up(a, b):
     try:
-        a + b
+        result = a + b
+        if type(result) == float:
+            return round(result, 3)
+        else:
+            return result
     except TypeError:
-        return f'{a}{b}'
-    else:
-        return a + b
+        return str(a) + str(b)
+
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
-print(add_everything_up(123.456, 7))
+print(add_everything_up(123.456, 7.0))
 print(add_everything_up('яблоко', 'Груша'))
