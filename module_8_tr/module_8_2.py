731a2d480374e7a85d70ea9d8f8c8c5d8c9c8721
IlyaFilev <rednif77@gmail.com>
2024-11-19 08:48:12 +0300

    Д/з "Сложные моменты и исключения в стеке вызовов функции"

--- module_8_tr/module_8_2.py
+++ module_8_tr/module_8_2.py
@@ -1,16 +1,23 @@
 # "Сложные моменты и исключения в стеке вызовов функции"
 def personal_sum(numbers):
     result = 0
+    incorrect_data = 0
     for i in numbers:
-        result += i
-    incorrect_data = 1
+        try:
+            result += i
+        except TypeError:
+            incorrect_data += 1
+            return f'Некорректный тип данных для подсчёта суммы - {i}'
     return result, incorrect_data
 
-def calculate_average(numbers): # Среднее арифметическое - сумма всех данных делённая на их количество.
-    return personal_sum(numbers)[0]/len(numbers)
+try:
+    def calculate_average(numbers): # Среднее арифметическое - сумма всех данных делённая на их количество.
+        a = 0
+except TypeError:
+    print('В numbers записан некорректный тип данных')
+    pass
+# else:
+    # return personal_sum(numbers)[0]/len(numbers)
 
 
-
-
-
-print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
+print(f'Результат 4: {calculate_average(42, 15, 36, 13)}') # Всё должно работать
