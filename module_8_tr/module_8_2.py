bf16f220de76255f9cceddd6cbf71c896152c4b7
IlyaFilev <rednif77@gmail.com>
2024-11-22 01:10:19 +0300

    Д/з "Сложные моменты и исключения в стеке вызовов функции"

--- module_8_tr/module_8_2.py
+++ module_8_tr/module_8_2.py
@@ -1,4 +1,5 @@
 # "Сложные моменты и исключения в стеке вызовов функции"
+
 def personal_sum(numbers):
     result = 0
     incorrect_data = 0
@@ -7,17 +8,25 @@ def personal_sum(numbers):
             result += i
         except TypeError:
             incorrect_data += 1
-            return f'Некорректный тип данных для подсчёта суммы - {i}'
+            print(f'Некорректный тип данных для подсчёта суммы - {i}')
     return result, incorrect_data
 
+
+def calculate_average(numbers):  # Среднее арифметическое всех чисел
+    try:
+        res_inc_data = personal_sum(numbers)
+        return res_inc_data[0]/(len(numbers)-res_inc_data[1])
+    except TypeError:
+        print(f'В numbers записан некорректный тип данных')
+    except ZeroDivisionError:
+        return 0
+
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(888)}') # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
 try:
-    def calculate_average(numbers): # Среднее арифметическое - сумма всех данных делённая на их количество.
-        a = 0
-except TypeError:
-    print('В numbers записан некорректный тип данных')
-    pass
-# else:
-    # return personal_sum(numbers)[0]/len(numbers)
-
-
-print(f'Результат 4: {calculate_average(42, 15, 36, 13)}') # Всё должно работать
+    print(f'Результат 5: {calculate_average(стока)}')
+except NameError:
+    print('Следи за тем, что вводишь, Бро!')
