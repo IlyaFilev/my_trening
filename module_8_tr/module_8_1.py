28e6079c307041227eb7eaa62a36e8e5289c8abc
IlyaFilev <rednif77@gmail.com>
2024-11-21 22:53:14 +0300

    Д/з "Try и Except"

--- module_8_tr/module_8_1.py
+++ module_8_tr/module_8_1.py
@@ -2,16 +2,12 @@
 
 def add_everything_up(a, b):
     try:
-        result = a + b
-        if type(result) == float:
-            return round(result, 3)
-        else:
-            return result
-    except TypeError:
+        return round((a + b), 3) # округление до 3 знака
+    except TypeError:  # одно из значений строка, то конвертируем оба значения в строки и складываем
         return str(a) + str(b)
 
 
-print(add_everything_up(123.456, 'строка'))
+print(add_everything_up(5, 6))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7.0))
-print(add_everything_up('яблоко', 'Груша'))
+print(add_everything_up('яблоко', [1, 'd', 5]))
