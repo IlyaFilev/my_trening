d44ca7cfaf1d5ae727cf1146498887a0802c1d2b
IlyaFilev <rednif77@gmail.com>
2024-12-05 00:55:21 +0300

    Д/з "Генераторы"

---
+++ module_9_func_prog/module_9_6.py
@@ -0,0 +1,24 @@
+'''Генераторы'''
+
+def all_variants(text):
+    for j in range(len(text)):
+        for i in range(len(text) - j):  # j возрастает => i убывает, возрастает кол. символов в выдаче: 1, 2, 3,..
+            res = text[i:i+j+1]         # Задаём начало и срез для строки (+1 для сдвига диапазона)
+            yield res
+
+
+a = all_variants("abcd")
+for i in a:
+    print(i)
+'''
+a
+b
+c
+d
+ab
+bc
+cd
+abc
+bcd
+abcd
+'''
